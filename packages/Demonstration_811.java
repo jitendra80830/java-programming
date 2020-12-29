6a642a4c16bdb18ae89a7ce3d8da891032468167
jitendra <jitendrakumar89973@gmail.com>
2020-12-29 17:26:25 +0530

    User defines packages programmings

---
+++ packages/Demonstration_811.java
@@ -0,0 +1,10 @@
+/*impoting package1 and package2*/
+class Demonstration_811{
+	public static void main(String[] args){
+		mypackage1.Myclass obj1=new mypackage1.Myclass();
+		mypackage2.Myclass ob2=new mypackage2.Myclass();
+		
+		obj1.msg();
+		ob2.msg();
+	}
+}
---
+++ packages/mypackage2/Myclass.java
@@ -0,0 +1,8 @@
+/*Save this frogram in package2*/
+package mypackage2;
+
+public class Myclass{
+	public void msg(){
+		System.out.println("i am from package2 classes");
+	}
+}
