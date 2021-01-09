e4ce245dd2a1ceb1a30d846a4d0a2012f179396c
jitendra <jitendrakumar89973@gmail.com>
2021-01-09 13:08:45 +0530

    Applet programs

---
+++ Applet_in_java/AppletSkeleton.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.applet.*;
+public class AppletSkeleton extends Applet{
+	public void init(){}
+	public void start(){}
+	public void stop(){}
+	public void destroy(){}
+	public void paint(Graphics g){}
+}
+/*
+<html>
+<body>
+<applet width="300" height="300" code="AppletSkeleton.class">
+</applet>
+</body>
+</html>
+*/
---
+++ Applet_in_java/AppletSkeleton2.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.applet.*;
+public class AppletSkeleton2 extends Applet{
+}
+/*
+<html>
+	<body>
+		<applet width="400" height="400" code="AppletSkeleton2.class">
+		</applet>
+	</body>
+</htlm>
+*/
